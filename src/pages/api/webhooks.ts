e776cc19989a1caac113495ddc46faa2a9aacc64
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2022-02-25 09:37:46 -0300

    feat: saving event data

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -19,6 +20,8 @@ export const config = {
     bodyParser: false
   }
 }
+
+// stripe listen --forward-to localhost:3000/api/webhooks
 
 const relevantEvents = new Set([
   'checkout.session.completed'
@@ -41,7 +44,23 @@ export default async(req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log('Evento recebido', event);
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            )
+
+            break;
+          default:
+            throw new Error('Unhandled event.')
+        }
+      } catch (err) {
+        return res.json({ error: 'Webhook handler failed.'})
+      }
     }
 
     res.json({ received: true });
