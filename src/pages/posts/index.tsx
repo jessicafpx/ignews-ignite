b6ccdfeed424f9b65f0f72cc7b39c4b633a78515
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2022-03-25 08:46:38 -0300

    feat: consuming prismic api

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
+import Prismic from '@prismicio/client';
 
 export default function Posts() {
   return (
@@ -35,4 +38,21 @@ export default function Posts() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')] , {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 100.
+    }
+  );
+
+  console.log(response);
+
+  return {
+    props: {}
+  }
 }
