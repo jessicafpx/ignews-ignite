4f60f9d79a793943b3118ee17412d2b5b4697968
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2022-03-29 08:45:50 -0300

    feat: add link component for menu navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
@@ -5,10 +7,14 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <img src="/images/logo.svg" alt="ig.news logo" />
         <nav>
-          <a href="" className={styles.active}>Home</a>
-          <a href="">Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a >Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
