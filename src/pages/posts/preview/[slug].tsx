3424d5a28722d874ca8bb02ab8ed8fd813cab66a
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2022-04-06 09:27:54 -0300

    style: add gradient in post preview

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -26,7 +26,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
           <h1>{post.title}</h1>
           <time>{post.updatedAt}</time>
           <div
-            className={styles.postContent}
+            className={`${styles.postContent} ${styles.previewContent}`}
             dangerouslySetInnerHTML={{ __html: post.content }}
          />
         </article>
