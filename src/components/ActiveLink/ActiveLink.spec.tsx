b9c4caa8bc8242aa25a201db407da45d176899b5
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2022-04-18 08:46:23 -0300

    refactor: use screen

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { ActiveLink } from "../ActiveLink"
 
 jest.mock('next/router', () => {
@@ -14,7 +14,8 @@ jest.mock('next/router', () => {
 
 describe('ActiveLink component', () => {
   it('renders correctly', () => {
-    const { debug, getByText } = render(
+    // const { debug, getByText } = render(
+    render(
       <ActiveLink href="/" activeClassName="active">
         <a>Home</a>
       </ActiveLink>
@@ -22,16 +23,16 @@ describe('ActiveLink component', () => {
 
     // debug();
 
-    expect(getByText('Home')).toBeInTheDocument()
+    expect(screen.getByText('Home')).toBeInTheDocument()
   })
 
   it('adds active class if the link as currently active', () => {
-    const { getByText } = render(
+    render(
       <ActiveLink href="/" activeClassName="active">
         <a>Home</a>
       </ActiveLink>
     )
 
-    expect(getByText('Home')).toHaveClass('active')
+    expect(screen.getByText('Home')).toHaveClass('active')
   })
 })
